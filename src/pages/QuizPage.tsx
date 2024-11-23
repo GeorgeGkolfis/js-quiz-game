95f1d65459037bfea01f350dd96ac9cad3624794
George Gkolfis <georgiosgolfis@gmail.com>
2024-11-23 14:39:56 +0200

    Create QuizPage.tsx

---
+++ src/pages/QuizPage.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header.js";
+import Quiz from "../components/Quiz.js";
+
+export default function QuizPage() {
+    return (
+        <>
+            <Header />
+            <main>
+                <Quiz />
+            </main>
+        </>
+    );
+}
